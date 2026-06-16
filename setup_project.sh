356266f5c1c9881c1a8c98126b84938c63c406e2
gg-prince <g.prince@alustudent.com>
2026-06-16 02:03:39 -0700

    Added the UPDATE THRESHOLDS feature

--- setup_project.sh
+++ setup_project.sh
@@ -21,16 +21,58 @@ cp -v "assets.csv" "config.json" "attendance_tracker_$at_dir/Helpers/"
 cp -v "reports.log" "attendance_tracker_$at_dir/reports"
 echo "Files Added"
 
+echo "------------------------------------------------------------"
+
 updte=""
 
-until [ $updte == "Y" ] || [ $updte == "n" ]
+until [ "$updte" == "Y" ] || [ "$updte" == "n" ]
 do
+	echo ""
 	read -rp "Do you want to update attendance thresholds(Y/n): " updte
-	if [ ! $updte == "Y" ] || [ ! $updte == "n" ]; then
+	if [ ! $updte == "Y" ] && [ ! $updte == "n"]; then
 		echo "Follow format (Y/n)"
 	fi
 done
 
-if [ $updte == "Y" ]; then
-	
+if [ "$updte" == "Y" ]; then
+
+	warning=""
+	failure=""
+
+	until [[ "$warning" =~ ^[0-9]*\.?[0-9]+$ ]]
+	do
+		echo ""
+	        read -rp "Enter new Warning threshold: " warning
+	        if [[ ! "$warning" =~ ^[0-9]*\.?[0-9]+$ ]]; then
+	                echo "Please enter a positive number"
+	        fi
+	done
+
+	sed -i -E "s/(\"warning\"[[:space:]]*:[[:space:]]*)[0-9]+(\.[0-9]+)?/\1$warning/" attendance_tracker_$at_dir/Helpers/config.json
+
+	echo "Updated Warning threshold."
+
+	until [[ "$failure" =~ ^[0-9]*\.?[0-9]+$ ]]
+        do
+                echo ""
+                read -rp "Enter new Failure threshold: " failure
+                if [[ ! "$failure" =~ ^[0-9]*\.?[0-9]+$ ]]; then
+                        echo "Please enter a positive number"
+                fi
+        done
+
+        sed -i -E "s/(\"failure\"[[:space:]]*:[[:space:]]*)[0-9]+(\.[0-9]+)?/\1$failure/" attendance_tracker_$at_dir/Helpers/config.json
+
+        echo "Updated Failure threshold."
+
 fi
+
+echo "------------------------------------------------------------"
+
+
+
+
+
+
+
+
